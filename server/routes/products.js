2b8a326ff1ff55709b0123492d716b9484ca5d8d
amilacodeolima <amilacodeolima@gmail.com>
2024-04-02 11:24:39 +0530

    Add Error handle for Product List API

--- server/routes/products.js
+++ server/routes/products.js
@@ -5,8 +5,14 @@ var router = express.Router();
 // List Products 
 router.get('/', function(req, res, next) {
   
-    const products = DUMMY_PRODUCT_LIST
-    return res.status(200).json(products)
+    try{
+        // Start : Should replace by Actual DB Query 
+        const products = DUMMY_PRODUCT_LIST
+        // End
+        return res.status(200).json(products)
+    }catch(e){
+        res.status(500).json()
+    }
 
 });
 
