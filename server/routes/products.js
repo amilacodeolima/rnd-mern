6b4b287ee8bf5262723b0e51b265c2f031410230
amilacodeolima <amilacodeolima@gmail.com>
2024-04-02 11:44:01 +0530

    Add Prodcut Create API endpoint and add uuid lib

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const { DUMMY_PRODUCT_LIST } = require('../dummy/dummy-products');
 var router = express.Router();
 
@@ -21,7 +22,9 @@ router.get('/:id', function(req, res, next) {
     
     try{
         const id = req.params.id;
+        // Start : Should replace by Actual DB Query
         const product = DUMMY_PRODUCT_LIST.find((item) => item._id === id)
+        // End
         if(product){
             return res.status(200).json(product)
         }else{
@@ -34,6 +37,29 @@ router.get('/:id', function(req, res, next) {
 });
 
 // Create Product
+router.post('/', function(req, res, next) {
+    
+    try{
+        const body = req.body;
+        const name = body.name;
+        const price = body.price;
+        if(name && price){
+            const newProduct = { name: body.name, price : body.price }
+            // Start : Should replace by Actual DB Query
+            const finalobj = { _id : uuidv4(), ...newProduct}
+            DUMMY_PRODUCT_LIST.push(finalobj)
+            const product =  finalobj;
+            // End
+            // TODO : if product already exsist in db should return 409 
+            return res.status(200).json(product)
+        }else{
+            return res.status(400).json()
+        }
+    }catch(err){
+        return res.status(500).json(err)
+    }
+
+});
 
 // Product Update by Id 
 
