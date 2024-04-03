5eaa52fd8e6e31f9e32bfb297c8b1e11a7a7d805
amilacodeolima <amilacodeolima@gmail.com>
2024-04-03 12:10:34 +0530

    api request move apiService.js

---
+++ client/src/Services/apiService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1'; // Default URL in case environment variable is missing
+
+const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+// TODO : Add Pagination
+export const fetchProducts = () => apiClient.get('/products');
+export const fetchProductById = (id) => apiClient.get(`/products/${id}`);
+// TODO : Product -> Create, Update, Delete
