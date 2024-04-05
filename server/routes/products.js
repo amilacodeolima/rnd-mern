044079230c8f2acd4e7dc8bce2e35f8b3d4e65c1
amilacodeolima <amilacodeolima@gmail.com>
2024-04-05 10:01:38 +0530

    validateReqPayload move util Use in Create and Update API

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Products = require('../models/products')
+var { validateRequestPayload } = require('../util/validateRequestPayload')
 
 // List Products 
 router.get('/', async (req, res, next) => {
@@ -32,39 +33,11 @@ router.get('/:id', async (req, res, next) => {
 
 });
 
-// Com Method
-/**
- * 
- * @param {*} reqPayload 
- * @param {*} validationConfig 
- * @returns Array
- */
-const validateRequestPayload = (reqPayload = {}, validationConfig = []) => {
-
-    const isError = [];
-    // const name = reqPayload.name
-    // const price = reqPayload.price 
-    // if(!name) {
-    //     isError = isError + `Name is Required`
-    // }
-    // if(!price){
-    //     isError = isError + `Price is Required`
-    // }
-    const vConfigLength =  validationConfig.length;
-    for(let i  = 0; i < vConfigLength; i++){
-        const key = validationConfig[i]
-        const isKeyFound = reqPayload[key];
-        if(!isKeyFound){
-            isError.push({
-                key, 
-                error: `${key} is Required`
-            })
-        }
-    }
-    return isError;
-}
-
 // Create Product
+const validationConfigCreateObj = [
+   { key: "name", type: "string", isRequired: true },
+   { key: "price", type: "number", isRequired: true }
+];
 router.post('/', async (req, res, next) => {
     
     // req.params eg : /products/:id = /products/1234 ->    id = req.params.id     =>  id = 1234
@@ -72,12 +45,11 @@ router.post('/', async (req, res, next) => {
     // req.body   eg : { name : 'abc'} ->                   name = req.body.name   => name = "abc"
 
     try{
-        const { name, price } = req.body;
+    
         // TODO : Use lib for paylod validation
-        const validationConfig = ["name", "price"];
-        const isError = validateRequestPayload(req.body, validationConfig)
+        const isError = validateRequestPayload(req.body, validationConfigCreateObj)
         if(!isError.length){
-            const newProduct = new Products({ name, price })
+            const newProduct = new Products(req.body)
             await newProduct.save()
             // TODO : if product already exsist in db should return 409 
             return res.status(200).json(newProduct)
@@ -91,6 +63,10 @@ router.post('/', async (req, res, next) => {
 });
 
 // Product Update by Id 
+const validationConfigUpdateObj = [
+    { key: "name", type: "string", isRequired: false },
+    { key: "price", type: "number", isRequired: false }
+ ];
 router.put('/:id', async (req, res, next) => {
     
     // req.params eg : /products/:id = /products/1234 ->    id = req.params.id     =>  id = 1234
@@ -99,18 +75,18 @@ router.put('/:id', async (req, res, next) => {
 
     try{
         const id = req.params.id
-        const { name, price } = req.body;
-        if(name && price && !isNaN(price)){
+        const isError = validateRequestPayload(req.body, validationConfigUpdateObj)
+        if(!isError.length){
             const product = await Products.findOne({_id : id }).exec() 
-            if(product){
-                const updateProduct = await Products.findOneAndUpdate({ _id: id }, { name, price }, { new: true } )
+            if(product){                
+                const updateProduct = await Products.findOneAndUpdate({ _id: id }, req.body, { new: true } )
                 // TODO : if product already exsist in db should return 409 
                 return res.status(200).json(updateProduct)
             }else{
                 return res.status(404).json()
             }
         }else{
-            return res.status(400).json()
+            return res.status(400).json(isError)
         }
     }catch(err){
         return res.status(500).json()
@@ -123,7 +99,7 @@ router.delete('/:id', async (req, res, next) => {
     
     try{
         const id = req.params.id;
-        const product = await Products.findOne({_id : _id }).exec() 
+        const product = await Products.findOne({_id : id }).exec() 
         if(product){
             await Products.deleteOne({ _id : product._id })
             return res.status(200).json()
